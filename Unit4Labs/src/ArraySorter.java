300d23519cc806952253b60aa244d1f317e6e0b8
kylea <kylea@DESKTOP-307U1LE.fios-router.home>
2019-12-24 18:59:32 -0500

    Added files for each question.

---
+++ Unit4Labs/src/ArraySorter.java
@@ -0,0 +1,9 @@
+
+public class ArraySorter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Unit4Labs/src/MatrixCalculator.java
@@ -0,0 +1,9 @@
+
+public class MatrixCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
