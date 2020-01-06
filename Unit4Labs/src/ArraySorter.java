2512bf3c75673b871b597eee940839f237d8d8f1
Glorp <Glorp@DESKTOP-81T6M7M.fios-router.home>
2020-01-06 00:44:23 -0500

    Declaring and initializing array

--- Unit4Labs/src/ArraySorter.java
+++ Unit4Labs/src/ArraySorter.java
@@ -1,8 +1,17 @@
-
+//Coded by Stephen Colandro
+import java.util.*;
 public class ArraySorter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Getting total number of elements from user
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number of elements in your array: ");
+		int num = s.nextInt();
+		
+		//Declare's and initializes array
+		int arr[] = new int[num];
+		
 	}
 
 }
