35a1f8bd6026022d34b275fbb89beda3cfdba5a2
Glorp <Glorp@DESKTOP-81T6M7M.fios-router.home>
2020-01-07 04:04:02 -0500

    Array Sorter completed

--- Unit4Labs/src/ArraySorter.java
+++ Unit4Labs/src/ArraySorter.java
@@ -11,7 +11,30 @@ public class ArraySorter {
 		
 		//Declare's and initializes array
 		int arr[] = new int[num];
+		int i, j, temp = 0;
 		
+		//Gets all elements in the array from user
+		System.out.println("Enter the " + num + " elements in your array: ");
+		for (i=0; i<num; i++) {
+			arr[i]=s.nextInt();
+		}
+		
+		//Arranges the elements in the array in ascending order
+		for (i=0; i<num; i++) {
+			for (j = i + 1; j<num; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		//Prints the array in ascending order
+		System.out.println("Your array in ascending order is: ");
+		for (i = 0; i<num; i++) {
+			System.out.print(" " + arr[i]);
+		}
 	}
 
 }
